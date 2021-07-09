c8af814fdf47839371c994f9d434d9d8a47a02c8
MattFH <2019302100016@whu.edu.cn>
2021-07-09 11:36:10 +0800

    hr Controller增加通过获得特定员工在本公司的档案信息接口

--- src/main/java/com/foe/talentmanagementback/controller/T_hrController.java
+++ src/main/java/com/foe/talentmanagementback/controller/T_hrController.java
@@ -4,6 +4,8 @@ package com.foe.talentmanagementback.controller;
 import com.foe.talentmanagementback.entity.Archive;
 import com.foe.talentmanagementback.entity.T_archive_detail;
 import com.foe.talentmanagementback.service.impl.T_archive_detailServiceImpl;
+import com.foe.talentmanagementback.service.impl.T_evaluation_detailsServiceImpl;
+import com.foe.talentmanagementback.service.impl.T_work_attendanceServiceImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -12,7 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -29,15 +30,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class T_hrController {
 
     @Autowired
-    private T_archive_detailServiceImpl archive_detailService;
+    private T_archive_detailServiceImpl archiveDetailService;
+    @Autowired
+    private T_evaluation_detailsServiceImpl evaluationDetailsService;
+    @Autowired
+    private T_work_attendanceServiceImpl workAttendanceService;
 
     @GetMapping("/talentArchive/{talentId}/{companyId}")
     @ApiOperation(value = "查询特定公司特定员工档案")
     public Archive getAtchiveOfOneTalentInCo(@ApiParam(value = "系统人才id，公司id", required = true) @PathVariable("talentId") int talentId,
                                              @PathVariable("companyId") int companyId) {
         Archive archive = new Archive();
-        archive.setArchiveDetail(archive_detailService.getArchiveBytIdWcId(talentId, companyId));
-        return null;
+        T_archive_detail archiveDetail = archiveDetailService.getArchiveBytIdWcId(talentId, companyId);
+        //调用archiveDetailService中getArchiveBytIdWcId给档案对象Archive中archiveDetail赋值
+        archive.setArchiveDetail(archiveDetail);
+        archive.setEvaluationDetails(evaluationDetailsService.getEvaluationsByADId(archiveDetail.getId()));
+        archive.setWorkAttendances(workAttendanceService.getAttendancesByADId(archiveDetail.getId()));
+        return archive;
     }
 
 }
