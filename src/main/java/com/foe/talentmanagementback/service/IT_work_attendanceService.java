16de63db91f3c485c9b3ccdb4e5bd29fca82e678
MattFH <2019302100016@whu.edu.cn>
2021-07-09 11:35:07 +0800

    attendance Controller增加通过特定档案号获得考情信息接口

--- src/main/java/com/foe/talentmanagementback/service/IT_work_attendanceService.java
+++ src/main/java/com/foe/talentmanagementback/service/IT_work_attendanceService.java
@@ -1,7 +1,10 @@
 package com.foe.talentmanagementback.service;
 
+import com.foe.talentmanagementback.entity.T_evaluation_details;
 import com.foe.talentmanagementback.entity.T_work_attendance;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +16,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IT_work_attendanceService extends IService<T_work_attendance> {
 
+    //通过archiveDetailId获得考勤记录链表
+    public List<T_work_attendance> getAttendancesByADId(int archiveDetailId);
 }
--- src/main/java/com/foe/talentmanagementback/service/impl/T_work_attendanceServiceImpl.java
+++ src/main/java/com/foe/talentmanagementback/service/impl/T_work_attendanceServiceImpl.java
@@ -1,10 +1,15 @@
 package com.foe.talentmanagementback.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.foe.talentmanagementback.entity.T_evaluation_details;
 import com.foe.talentmanagementback.entity.T_work_attendance;
 import com.foe.talentmanagementback.mapper.T_work_attendanceMapper;
 import com.foe.talentmanagementback.service.IT_work_attendanceService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +22,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class T_work_attendanceServiceImpl extends ServiceImpl<T_work_attendanceMapper, T_work_attendance> implements IT_work_attendanceService {
 
+    @Autowired
+    private T_work_attendanceMapper workAttendanceMapper;
+
+    @Override
+    public List<T_work_attendance> getAttendancesByADId(int archiveDetailId) {
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.eq("archive_detail_id",archiveDetailId);
+        return workAttendanceMapper.selectList(queryWrapper);
+    }
 }
