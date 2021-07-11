365c0eea57d28e1a4d950777bfc45b2be0ebbfa2
MattFH <2019302100016@whu.edu.cn>
2021-07-11 10:33:15 +0800

    中午提交

--- src/main/java/com/foe/talentmanagementback/handler/MyMetaObjectHandler.java
+++ src/main/java/com/foe/talentmanagementback/handler/MyMetaObjectHandler.java
@@ -1,4 +1,4 @@
-package com.foe.talentmanagementback.Handler;
+package com.foe.talentmanagementback.handler;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import lombok.extern.slf4j.Slf4j;
